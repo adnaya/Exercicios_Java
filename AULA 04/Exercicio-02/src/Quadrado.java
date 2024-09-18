e41ca30df69984ee1163f7e90176ea71af889a5f
DSM2 <fatec-dsm2@academico.intra>
2024-09-17 22:23:50 -0300

    aula04 ex2 comm

--- AULA 04/Exercicio-02/src/Quadrado.java
+++ AULA 04/Exercicio-02/src/Quadrado.java
@@ -8,5 +8,44 @@
  * @author fatec-dsm2
  */
 public class Quadrado {
+    private double areaQuadrado;
+    private double perimetroQuadrado;
+
+    public Quadrado() {
+        this(0.00,0.00);
+    }
+
+    public Quadrado(double areaQuadrado, double perimetroQuadrado) {
+        this.areaQuadrado = areaQuadrado;
+        this.perimetroQuadrado = perimetroQuadrado;
+    }
+
+    public double getAreaQuadrado() {
+        return areaQuadrado;
+    }
+
+    public void setAreaQuadrado(double areaQuadrado) {
+        this.areaQuadrado = areaQuadrado;
+    }
+
+    public double getPerimetroQuadrado() {
+        return perimetroQuadrado;
+    }
+
+    public void setPerimetroQuadrado(double perimetroQuadrado) {
+        this.perimetroQuadrado = perimetroQuadrado;
+    }
     
+    public double calcularArea(){
+        
+        return 
+    }
+    
+    public double calcularPerimetro(){
+    
+    }
+    
+    public void mostrarValores(){
+    
+    }
 }
