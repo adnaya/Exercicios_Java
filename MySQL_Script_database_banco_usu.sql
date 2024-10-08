29bd0be61990273d655cefb50f9c80d4f9b16f6a
Amanda <amanda.fralve@gmail.com>
2024-10-08 12:04:36 -0300

    script mysql att

---
+++ MySQL_Script_database_banco_usu.sql
@@ -0,0 +1,22 @@
+create database banco_usu;
+use banco_usu;
+
+create table UsuarioUm(
+id int auto_increment primary key,
+nome varchar(50),
+email varchar(50),
+login varchar(20),
+senha varchar(50)
+);
+
+create table Produto(
+codigo int auto_increment primary key,
+nome varchar(50),
+descricao varchar(200)
+);
+
+create table Venda(
+codigo int auto_increment primary key,
+NomeVendedor varchar(50),
+NomeProduto varchar(50)
+);
